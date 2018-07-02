63f9e2554f9cf094c421d5027e58db6a926343d1
DESKTOP-SFD73BN\DNZ <dnz7993@sjtu.edu.cn>
2018-07-02 15:56:21 +0800

    header bar

---
+++ src/module/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import HeaderMenu from './HeaderMenu'
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <HeaderMenu/>
+      </div>
+    );
+  }
+}
+
+export default App;
