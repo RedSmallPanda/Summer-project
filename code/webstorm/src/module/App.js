574d390330f1e319e7a3be2eb28b0d28ff302d46
gyh <RemGYH@sjtu.edu.cn>
2018-07-06 17:34:20 +0800

    modify App.js

--- code/webstorm/src/module/App.js
+++ code/webstorm/src/module/App.js
@@ -11,7 +11,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <GoodDetailPage/>
+          <HeaderMenu/>
+          <MyRouter/>
       </div>
   );
   }
