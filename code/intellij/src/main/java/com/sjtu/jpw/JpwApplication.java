7a2e8113404fa64cc33dba6a76a1d9dbd59b6342
saltyfishfish <mytkeroro@sjtu.edu.cn>
2018-07-11 14:25:27 +0800

    backend framework

---
+++ code/intellij/src/main/java/com/sjtu/jpw/JpwApplication.java
@@ -0,0 +1,12 @@
+package com.sjtu.jpw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpwApplication.class, args);
+    }
+}
