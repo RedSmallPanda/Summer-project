9f4a0d2051aff6325e9354f0c5c420da8f71cc22
DESKTOP-SFD73BN\DNZ <dnz7993@sjtu.edu.cn>
2018-07-19 14:10:01 +0800

    add vcode

--- code/webstorm/src/module/MainPages/Login.js
+++ code/webstorm/src/module/MainPages/Login.js
@@ -1,11 +1,55 @@
 import React from 'react';
-import {Button, Modal, Form, Input,Icon,} from 'antd'
+import {Button, Modal, Form, Input,Icon,Row, Col} from 'antd'
+import VCode from "./VCode"
 import "../../css/App.css"
 
 
 const FormItem = Form.Item;
 const Login = Form.create()(
     class extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                ...this.initState(),
+                result:"",
+                refresh: false
+            };
+        }
+
+        initState(){
+            return {
+                data: this.getRandom(109,48,4),
+            }
+        }
+
+        handleChange = () =>{
+            this.setState({
+                ...this.initState(),
+            })
+        };
+
+        getRandom(max, min, num) {
+            const asciiNum = ~~(Math.random()*(max-min+1)+min);
+            if(!Boolean(num)){
+                return asciiNum
+            }
+            const arr = [];
+            for(let i = 0; i < num; i++){
+                arr.push(this.getRandom(max, min))
+            }
+            return arr;
+        }
+
+        setResult = (arr) =>{
+            let result = "";
+            for(let i = 0; i < 4; i++){
+                let v;
+                v = arr[i];
+                result += String.fromCharCode(v > 57 && v < 84 ? v + 7 : ( v < 57 ? v : v + 13 ));
+            }
+            return result;
+        };
+
         //自定义 validator函数
         username_validate=(rule,value,callback)=>{
             //   const form = this.formRef.props.form;
@@ -25,38 +69,72 @@ const Login = Form.create()(
                     footer={null}
                     maskClosable={false}
                     destroyOnClose={true}
+                    width="400px"
                 >
                     <div className="padding">
                         <p className="letters">请登录</p>
-                    <Form layout="vertical" >
-                        <FormItem label={null}>
-                            {getFieldDecorator('username', {
-                                rules: [
-                                    { required: true, message: '请输入用户名' },
-             //                       { max:12,message:'用户名长度不超过12', },
-                                    {validator:(rule,value,callback)=>{
-                                        //   const form = this.formRef.props.form;
-                                        //value's type need to transform
-                                        if(String(value).length>10){callback("username >10 ");}
-                                        else {callback()}
+                        <Form layout="vertical" >
+                            <FormItem label={null}>
+                                {getFieldDecorator('username', {
+                                    rules: [
+                                        { required: false, message: '请输入用户名' },
+                                        //                       { max:12,message:'用户名长度不超过12', },
+                                        {validator:(rule,value,callback)=>{
+                                            //   const form = this.formRef.props.form;
+                                            //value's type need to transform
+                                            if(String(value).length>10){callback("username >10 ");}
+                                            else {callback()}
 
-                                    }},
+                                        }},
 
-                                ],
-                                validateTrigger:'onBlur',
-                            })(
-                                <p>
-                                <Input className="input"  placeholder="Enter your username"
-                                       prefix={<Icon type="user"   />}/>
-                                </p>
-                            )}
-                        </FormItem>
-                        <FormItem label={null}>
-                            {getFieldDecorator('password',{rules:[{required:true,}]})(<p><Input className="input"  placeholder="Enter your password" prefix={<Icon type="lock" />}  type="password" /></p>)}
-                        </FormItem>
-                        <Button type="primary" onClick={onLogin}> 登录 </Button>
-                        <FormItem/>
-                    </Form>
+                                    ],
+                                    validateTrigger:'onBlur'
+                                })(
+                                    <p>
+                                        <Input className="input"  placeholder="请输入用户名"
+                                               prefix={<Icon type="user"   />}/>
+                                    </p>
+                                )}
+                            </FormItem>
+                            <FormItem label={null}>
+                                {getFieldDecorator('password',{
+                                    rules:[{
+                                        required:false,
+                                    }]
+                                })(
+                                    <p>
+                                        <Input className="input"  placeholder="请输入密码" prefix={<Icon type="lock" />}  type="password" />
+                                    </p>
+                                )}
+                            </FormItem>
+                            <FormItem label={null}>
+                                {getFieldDecorator('vCode',{
+                                    rules:[{
+                                        required:false,
+                                    },{
+                                        validator:(rule,value,callback)=>{
+                                            //   const form = this.formRef.props.form;
+                                            //value's type need to transform
+                                            if(String(value).toLowerCase() !== this.setResult(this.state.data).toLowerCase()){callback("验证码错误");}
+                                            else {callback()}
+                                        }
+                                    }],
+                                    validateTrigger:'onChange',
+                                })(<p>
+                                    <Row>
+                                        <Col span={15}>
+                                            <Input placeholder="请输入验证码" prefix={<Icon type="safety" />}/>
+                                        </Col>
+                                        <Col span={2}/>
+                                        <VCode
+                                            data={this.state.data}
+                                            handleChange={this.handleChange}/>
+                                    </Row>
+                                </p>)}
+                            </FormItem>
+                            <Button type="primary" onClick={onLogin}> 登录 </Button>
+                            <FormItem/>
+                        </Form>
                     </div>
                 </Modal>
             );
